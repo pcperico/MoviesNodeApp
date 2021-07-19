f2450c1953d78e4f886f3530afba7f2007331782
Arturo Salinas <salina01@stepstone.com>
2021-07-19 15:45:17 -0600

    adding nodemon

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/api/categories', (req, res) => {
 });
 
 app.listen(8000, () => {
-  console.log('Example app listening on port 8000!')
+  console.log('Example app listening on port 8000!');
 });
