5dfc90dbfd1705627fa52cd83c591f9624bb6101
Arturo Salinas <salina01@stepstone.com>
2021-07-19 15:31:30 -0600

    adding moke endpoint to start build react components for list categories

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const categories = [
+    {id:1, name:"drama"},
+    {id:2, name:"terror"},
+    {id:3, name:"romance"},
+];
+
+app.get('/api/categories', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(categories));
 });
 
 app.listen(8000, () => {
